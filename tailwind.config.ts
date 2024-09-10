82617d6dfa6bf3c0e3953a49a2133505e82e6e13
Nguyễn Quốc Hùng <nguyenhung016446@gmai.com>
2024-09-10 21:59:46 +0700

    Modify Tailwind configuration file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,19 +2,13 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
    content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./Pages/**/*.{js,ts,jsx,tsx}",
-    "./Components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-    screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
-    },
-  },
+      },
 },
   plugins: [],
 };
